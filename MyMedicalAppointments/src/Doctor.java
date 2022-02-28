7dc61e8e14b2ff859a2fb883a9de986fce7c2b53
Jorge Vásquez <jas.vasquezc@gmail.com>
2022-02-27 22:32:18 -0600

    Iniciando el proyecto con algunas clases.

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -1,11 +1,21 @@
 public class Doctor {
 
-    int id;
+    //Atributos
+    static int id = 0; //Autoincrement
     String name;
     String speciality;
+
+    Doctor(){
+        System.out.println("Construyendo el Objeto Doctor");
+        id++;
+    }
 
     //Comportamientos
     public void showName(){
         System.out.println("Name: " + name);
+    }
+
+    public void showId(){
+        System.out.println("ID Doctor: " + id);
     }
 }
--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -1,9 +1,17 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
 
         Doctor myDoctor = new Doctor();
         myDoctor.name = "Luis Curán";
         myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor myDocotrAnn = new Doctor();
+        myDoctor.showId();
+
+        showMenu();
 
     }
 }
