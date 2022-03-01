d963fdd4e8f8a26fe2d3ad8c61e2423c2f536545
Jorge Vásquez <jas.vasquezc@gmail.com>
2022-02-28 18:55:15 -0600

    Iniciando el proyecto con algunas clases.

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -3,15 +3,6 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Luis Curán";
-        myDoctor.showName();
-        myDoctor.showId();
-
-        Doctor myDocotrAnn = new Doctor();
-        myDoctor.showId();
-
-        //Llamando un método estático
         showMenu();
 
     }
