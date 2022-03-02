3d14fbc7b985e69dfd4bf091624dbe676053ca2e
Jorge Vásquez <jas.vasquezc@gmail.com>
2022-03-01 22:03:32 -0600

    Update

---
+++ MyMedicalAppointments/src/Ambulancia.java
@@ -0,0 +1,13 @@
+public class Ambulancia {
+
+    Doctor doctor;
+    Patient patient;
+    Enfermera enfermera;
+
+    Ambulancia(Doctor doctor, Patient patient, Enfermera enfermera){
+        this.doctor = doctor;
+        this.patient = patient;
+        this.enfermera = enfermera;
+
+    }
+}
