54d145e59023712553150bbb13c5098d0381c80b
Jorge Vásquez <jas.vasquezc@gmail.com>
2022-03-01 21:59:49 -0600

    Update

---
+++ MyMedicalAppointments/src/Consultorio.java
@@ -0,0 +1,15 @@
+public class Consultorio {
+
+    Patient pacientes;
+    Doctor  doctor;
+    Enfermera enfermera;
+    Secretaria secretaria;
+
+    Consultorio(Patient pacientes, Doctor doctor, Enfermera enfermera, Secretaria secretaria){
+        this.pacientes = pacientes;
+        this.doctor = doctor;
+        this.enfermera = enfermera;
+        this.secretaria = secretaria;
+
+    }
+}
