ac9b00c85ead5bb985ab9286308d224db3cd9d2a
Jorge Vásquez <jas.vasquezc@gmail.com>
2022-03-01 22:01:20 -0600

    Update

--- MyMedicalAppointments/src/Hospital.java
+++ MyMedicalAppointments/src/Hospital.java
@@ -1,14 +1,16 @@
-public class Consultorio {
+public class Hospital {
 
     Patient pacientes;
     Doctor  doctor;
     Enfermera enfermera;
     Secretaria secretaria;
+    Consultorio consultorio;
 
-    Consultorio(Patient pacientes, Doctor doctor, Enfermera enfermera, Secretaria secretaria){
+    Hospital(Patient pacientes, Doctor doctor, Enfermera enfermera, Secretaria secretaria, Consultorio consultorio){
         this.pacientes = pacientes;
         this.doctor = doctor;
         this.enfermera = enfermera;
         this.secretaria = secretaria;
+        this.consultorio = consultorio;
     }
 }
