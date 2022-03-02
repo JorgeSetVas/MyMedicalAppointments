0b74ec94ee8648a967445bcf3cda9bf7a831a2bc
Jorge Vásquez <jas.vasquezc@gmail.com>
2022-03-01 22:04:55 -0600

    Update

--- MyMedicalAppointments/src/Secretaria.java
+++ MyMedicalAppointments/src/Secretaria.java
@@ -6,4 +6,20 @@ public class Secretaria {
         this.pacientes = pacientes;
         this.doctor = doctor;
     }
+
+    public Patient getPacientes() {
+        return pacientes;
+    }
+
+    public void setPacientes(Patient pacientes) {
+        this.pacientes = pacientes;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
 }
